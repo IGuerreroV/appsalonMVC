bbb383cec67033141d62ca7cf2ca3212a6600a1b
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-08-25 13:55:46 -0500

    chore: Agregar controlador de login y rutas relacionadas

---
+++ controllers/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Controllers;
+
+class loginController {
+    public static function login() {
+        echo 'Desde el controlador de login';
+    }
+
+    public static function logout() {
+        echo 'Desde el controlador de logout';
+    }
+
+    public static function olvide() {
+        echo 'Desde el controlador de olvide';
+    }
+
+    public static function recuperar() {
+        echo 'Desde el controlador de recuperar';
+    }
+    
+    public static function crear() {
+        echo 'Desde el controlador de crear';
+    }
+}
--- public/index.php
+++ public/index.php
@@ -3,10 +3,26 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
+use Controllers\loginController;
 
 $router = new Router();
 
+// Iniciar Sesión
+$router->get('/', [loginController::class, 'login']);
+$router->post('/', [loginController::class, 'login']);
 
+// Cerrar Sesión
+$router->get('/logout', [loginController::class, 'logout']);
+
+// Recuperar Password
+$router->get('/olvide', [loginController::class, 'olvide']);
+$router->post('/olvide', [loginController::class, 'olvide']);
+$router->get('/recuperar', [loginController::class, 'recuperar']);
+$router->post('/recuperar', [loginController::class, 'recuperar']);
+
+// Crear Cuenta
+$router->get('/crear-cuenta', [loginController::class, 'crear']);
+$router->post('/crear-cuenta', [loginController::class, 'crear']);
 
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
