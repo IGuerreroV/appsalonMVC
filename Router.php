faf92f159d39267dd57cd83753c655a4b93d6bc8
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-08-24 23:52:37 -0500

    chore: Agregar archivos y directorios iniciales

---
+++ Router.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace MVC;
+
+class Router {
+    public array $getRoutes = [];
+    public array $postRoutes = [];
+
+    public function get($url, $fn) {
+        $this->getRoutes[$url] = $fn;
+    }
+
+    public function post($url, $fn) {
+        $this->postRoutes[$url] = $fn;
+    }
+
+    public function comprobarRutas() {
+        // Proteger Rutas...
+        session_start();
+
+        $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        if($method === 'GET') {
+            $fn = $this->getRoutes[$currentUrl] ?? null;
+        } else {
+            $fn = $this->postRoutes[$currentUrl] ?? null;
+        }
+
+        if($fn) {
+            // Call user fn va a llamr una funcion cuadno no sabemos cual sera
+            call_user_func($fn, $this); // This es para pasar argumentos
+        } else {
+            echo 'Pagina no encontrada';
+        }
+    }
+
+    public function render($view, $datos = []) {
+        // Leer lo que le pasamos a la vista
+        foreach($datos as $key => $value) {
+            $$key = $value; // Doble signo de valor significa: variable variable, basicamente nuestra variable sigue siendo la original, pero al asignarle a otra no la reescribe, mantiene su valor, de esta forma el nombre de la variable se asigna dinamicamente
+        }
+
+        ob_start(); // Almacenamiento en memoria durante un momento...
+
+        // Entonces incluimos la vista en el layout
+        include_once __DIR__ . '/views/$view.php';
+        $contenido = ob_get_clean(); // Limpia el Buffer
+        include_once __DIR__ . '/views/layoyt.php';
+    }
+}
