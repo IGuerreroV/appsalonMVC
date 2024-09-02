f182980769c0830bf34086ceeca32f5a45cf7c1b
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-09-02 17:14:02 -0500

    feat: Añadiendo los Contenedores Principales

--- views/cita/index.php
+++ views/cita/index.php
@@ -1,2 +1,18 @@
 <h1 class="nombre-pagina">Crear Nueva Cita</h1>
-<p class="descripcion-pagina">Elige tus servicios a continuación</p>
+<p class="descripcion-pagina">Elige tus servicios y coloca tus datos</p>
+
+<div id="app">
+    <div class="seccion" id="paso-1">
+        <h2>Servicios</h2>
+        <p>Elige tus servicios a continuación</p>
+        <div class="listado-servicios" id="servicios"></div>
+    </div>
+    <div class="seccion" id="paso-2">
+        <h2>Tus Datos y Cita</h2>
+        <p>Coloca tus datos y fecha de tu cita</p>
+    </div>
+    <div class="seccion" id="paso-3">
+        <h2>Resumen</h2>
+        <p>Verifica que la información sea correcta</p>
+    </div>
+</div>
