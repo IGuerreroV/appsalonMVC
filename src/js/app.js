7db97eeb590273341b11017ded8a77b9c974559b
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-09-04 15:31:39 -0500

    feat: Primeros pasos con la Navegación por Tabs

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,27 @@
-alert('Desde app.js')
+let paso = 1
+
+document.addEventListener('DOMContentLoaded', function() {
+    iniciarApp()
+})
+
+function iniciarApp() {
+    
+    tabs() // Cambia la seccion cuando se presionen los tabs
+}
+
+function mostrarSeccion() {
+    console.log('Mostrando seccion');
+    
+}
+
+function tabs() {
+    const botones = document.querySelectorAll('.tabs button')
+    botones.forEach( boton => {
+        boton.addEventListener('click', (event) => {
+            paso = parseInt( event.target.dataset.paso )
+            mostrarSeccion()
+            
+        })
+    })
+    
+}
