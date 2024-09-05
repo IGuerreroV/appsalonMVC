8abbd6fd6ff1cdc7c4c74f784eda5a43979606bc
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-09-05 14:08:04 -0500

    feat: Finalizando la navegación por tabs

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function iniciarApp() {
+  mostrarSeccion(); // Muetsra y oculta las secciones segun el paso
   tabs(); // Cambia la seccion cuando se presionen los tabs
 }
 
@@ -19,6 +20,16 @@ function mostrarSeccion() {
   const pasoSelector = `#paso-${paso}`;
   const seccion = document.querySelector(pasoSelector);
   seccion.classList.add('mostrar');
+
+  // Eliminar la clase de actual en el tab anterior
+  const tabAnterior = document.querySelector('.actual')
+  if(tabAnterior) {
+    tabAnterior.classList.remove('actual')
+  }
+
+  // Resalta el tab actual
+  const tab = document.querySelector(`[data-paso="${paso}"]`)
+  tab.classList.add('actual');
 }
 
 function tabs() {
