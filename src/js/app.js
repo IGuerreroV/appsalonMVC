70151fe1e3a42ca9f532f8c140c9e6eea898b415
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-09-10 13:22:49 -0500

    feat: Añadir función para mostrar resumen de la cita
    Añade una nueva función llamada "mostrarResumen" que se encarga de mostrar el resumen de la cita. Esta función se invoca al iniciar la aplicación y también se llama cuando se cambia de paso en el formulario de la cita. El resumen muestra la cantidad de servicios seleccionados y verifica si faltan datos en la cita. Si faltan datos, se muestra un mensaje de error, de lo contrario, se muestra un mensaje de confirmación.
    
    Se modificaron los archivos:
    - public/build/js/app.js
    - src/js/app.js
    - views/cita/index.php

--- src/js/app.js
+++ src/js/app.js
@@ -25,6 +25,8 @@ function iniciarApp() {
   nombreCliente(); // Añade el nombre del cliente al objeto de la cita
   seleccionarFecha(); // Añade la fecha de la cita al objeto de la cita
   seleccionarHora(); // Añade la hora de la cita al objeto de la cita
+
+  mostrarResumen(); // Muestra el resumen de la cita
 }
 
 function mostrarSeccion() {
@@ -71,6 +73,7 @@ function botonesPaginador() {
   } else if (paso === 3) {
     paginaAnterior.classList.remove('ocultar');
     paginaSiguiente.classList.add('ocultar');
+    mostrarResumen();
   } else {
     paginaAnterior.classList.remove('ocultar');
     paginaSiguiente.classList.remove('ocultar');
@@ -209,4 +212,16 @@ function mostrarAlerta(mensaje, tipo) {
   setTimeout(() => {
     alerta.remove()
   }, 3000)
+}
+
+function mostrarResumen() {
+  const resumen = document.querySelector('.contenido-resumen')
+
+  console.log(cita.servicios.length);
+
+  if(Object.values(cita).includes('')) {
+    console.log('Hacen falta datos');
+  } else {
+    console.log('Todo correcto');
+  }
 }
