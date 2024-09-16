d2719976c14142ee2b5d6a0dbd7765b7ef9f90c6
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-09-16 13:40:09 -0500

    feat: Guardar los Servicios de la Cita en la Base de Datos

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -4,6 +4,7 @@ namespace Controllers;
 
 use Model\Servicio;
 use Model\Cita;
+use Model\CitaServicio;
 
 class APIController {
     public static function index() {
@@ -12,9 +13,24 @@ class APIController {
     }
 
     public static function guardar() {
-        
+
+        // Almacena la Cita y devuleve el ID
         $cita = new Cita($_POST);
         $resultado = $cita->guardar();
-        echo json_encode($resultado);
+
+        $id = $resultado['id'];
+
+        // Almacena los servicios con el ID de la cita
+        $idServicios = explode(",", $_POST['servicios']);
+        foreach($idServicios as $idServicio) {
+            $args = [
+                'cita_id' => $id,
+                'servicio_id' => $idServicio
+            ];
+            $citaServicio = new CitaServicio($args);
+            $citaServicio->guardar();
+        }
+
+        echo json_encode(['resultado' => $resultado]);
     }
 }
