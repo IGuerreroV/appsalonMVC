ac14a69a6978e1089c70be5ec0e051251b0e7025
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-09-18 18:28:09 -0500

    feat: Implementar función para consultar la base de datos en PHP

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -139,6 +139,13 @@ class ActiveRecord
         return array_shift($resultado);
     }
 
+    // Consulta Plana de SQL (Utilizar cuando los metodos del modelo no son suficientes)
+    public static function SQL($query)
+    {
+        $resultado = self::consultarSQL($query);
+        return $resultado;
+    }
+
     // Obtener Registros con cierta cantidad
     public static function get($limite)
     {
