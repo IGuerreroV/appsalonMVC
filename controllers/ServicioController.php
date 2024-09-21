c0cf68ff026ea00ca858b58032a28b66fb57d6a0
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-09-21 13:57:03 -0500

    feat: Añadir routing para CRUD de ServicioController

---
+++ controllers/ServicioController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+
+class ServicioController
+{
+    public static function index(Router $router)
+    {
+        $router->render('servicios/index', [
+
+        ]);
+    }
+
+    public static function crear(Router $router)
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            echo 'creando...';
+        }
+    }
+
+    public static function actualizar(Router $router)
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            echo 'actualizando...';
+        }
+    }
+
+    public static function eliminar(Router $router)
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            echo 'Eliminando...';
+        }
+    }
+}
