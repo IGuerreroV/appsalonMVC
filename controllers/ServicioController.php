4cc48310f21814c4e09046bb34eb7cea7a08a91b
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-09-23 17:02:12 -0500

    feat: Añadir vistas para los servicios
    Se pasa la variable `nombre` a las vistas renderizadas, recuperada del superglobal `$_SESSION`.

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -8,16 +8,22 @@ class ServicioController
 {
     public static function index(Router $router)
     {
+        isSession();
         $router->render('servicios/index', [
-
+            'nombre' => $_SESSION['nombre'],
         ]);
     }
 
     public static function crear(Router $router)
     {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        isSession();
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
             echo 'creando...';
         }
+
+        $router->render('servicios/crear', [
+            'nombre' => $_SESSION['nombre'],
+        ]);
     }
 
     public static function actualizar(Router $router)
@@ -25,6 +31,10 @@ class ServicioController
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             echo 'actualizando...';
         }
+
+        $router->render('servicios/actualizar', [
+            'nombre' => $_SESSION['nombre'],
+        ]);
     }
 
     public static function eliminar(Router $router)
